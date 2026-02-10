9b192feb7a35ef2989dc48a1a9a8e9f757afebff
Bien <benregidor@gmail.com>
2026-02-10 10:50:46 +0800

    1st Update

---
+++ Views/AttendanceLogsPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace AttendanceShiftingManagement.Views
+{
+    public partial class AttendanceLogsPage : UserControl
+    {
+        public AttendanceLogsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
